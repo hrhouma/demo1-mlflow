eaeb8056262f35b7d3e77a4718050d5cd11f8dc0
Haythem REHOUMA <rehoumahaythem@gmail.com>
2024-06-06 12:58:12 -0400

    Update train.py

--- train.py
+++ train.py
@@ -1,1 +1,7 @@
+import mlflow
+import mlflow.sklearn
 
+with mlflow.start_run():
+    mlflow.log_param("param1", value1)
+    mlflow.log_metric("metric1", value1)
+    mlflow.sklearn.log_model(model, "model")
